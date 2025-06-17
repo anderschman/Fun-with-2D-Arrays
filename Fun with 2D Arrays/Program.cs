7faf69e86885d06345ce188ad14777e36cd1dfe3
anderschman <henry.schulze@gmail.com>
2025-06-17 22:15:11 +0200

    Projektdateien hinzufügen.

---
+++ Fun with 2D Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Fun_with_2D_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fun with 2D Arrays
+            Console.WriteLine("Fun with 2D Arrays");
+
+            //User enters number of lines
+            Console.WriteLine("How many rows shall the grid have?");
+            int rows = int.Parse(Console.ReadLine());
+
+            //User enters number of columns
+            Console.WriteLine("How many columns shall the grid have?");
+            int columns = int.Parse(Console.ReadLine());
+
+            //Create 2D Array (with numbers)
+            string[,] Array2D = new string[rows, columns];
+
+            //Fill chessboard pattern alternately with LETTERS
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Array2D[i, j] = "X";
+                    }
+                    else
+                    {
+                        Array2D[i, j] = "O";
+                    }
+                }
+            }
+            // Edit grid
+            Console.WriteLine("\nGrid pattern:");
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(Array2D[i, j] + "|");
+                }
+                if (i < rows - 1)
+                {
+                    Console.WriteLine("\n" + new string('-', (columns * 2) + 1));
+                }
+            }
+        }
+    }
+}
